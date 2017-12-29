6ba5d23dbf661a73ff8322434a11be4fc31d4391
Ondřej satai Nekola <ondra@nekola.cz>
2017-12-29 11:11:07 +0100

    travis - test to break build

--- src/test/scala/cz/nekola/frozenbeagle/SexSpec.scala
+++ src/test/scala/cz/nekola/frozenbeagle/SexSpec.scala
@@ -6,7 +6,7 @@ import org.scalatest.prop.Checkers
 class SexSpec extends FunSpec with Matchers with Checkers {
   describe("Males and Females") {
     it("there are two different values of sex: F and M") {
-     F should not equal M
+     F should equal M // broken build
     }
   }
 }
