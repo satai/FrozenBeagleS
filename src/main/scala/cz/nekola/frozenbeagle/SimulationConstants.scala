fdbc21014abf08f5b7f842b9a91258be44767cf7
Ondra Nekola <ondra@nekola.cz>
2018-01-01 10:21:30 +0100

    All the Simulation Constants moved from haskell version

--- src/main/scala/cz/nekola/frozenbeagle/SimulationConstants.scala
+++ src/main/scala/cz/nekola/frozenbeagle/SimulationConstants.scala
@@ -1,9 +1,25 @@
 package cz.nekola.frozenbeagle
 
 object SimulationConstants {
-  val dimensionCount: Int = 4
+  lazy val dimensionCount: Int = 4
 
-  val zeroPhenotypeVec: List[Double] = List.fill(dimensionCount) (0.0)
+  lazy val zeroPhenotypeVec: List[Double] = List.fill(dimensionCount) (0.0)
 
-  val fitnessDecreaseCoefficient: Double = -0.005
+  lazy val optimumChangeGeneration: Int = 8 * 1024
+
+  lazy val maxSteps: Int = 3 * optimumChangeGeneration
+
+  lazy val accidentDeathProbability: Double = 0.0
+
+  lazy val probabilityAlleleMutation: Double = 0.0002
+
+  lazy val optimumSizeCoefficient: Double = 12.0
+
+  lazy val optimumChangeSizeCoefficient: Double = 12.0
+
+  lazy val fitnessDecreaseCoefficient: Double = -0.005
+
+  lazy val negativeDominanceScale: Double = 1.5
+
+  lazy val negativeDominanceShape: Double = 1.5
 }
