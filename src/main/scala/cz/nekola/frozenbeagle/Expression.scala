4529bd3dd9fe28921800ca13493fd75532c03c03
Ondřej satai Nekola <ondra@nekola.cz>
2018-01-21 13:22:00 +0100

    minor: reformat

--- src/main/scala/cz/nekola/frozenbeagle/Expression.scala
+++ src/main/scala/cz/nekola/frozenbeagle/Expression.scala
@@ -7,7 +7,9 @@ object Expression{
     val dna1 = dnaStrings._1
     val dna2 = dnaStrings._2
 
-    val changes = dna1.genes.zip(dna2.genes).map { t => if (t._1 == t._2) t._1.dominantEffect else (t._1.effect + t._2.effect)}
+    val changes = dna1.genes.zip(dna2.genes).map { t => if (t._1 == t._2) t._1.dominantEffect
+                                                        else t._1.effect + t._2.effect
+                                                 }
     changes.foldLeft(zeroPhenotype){ (p: Phenotype, pc: PhenotypeChange) => p + pc}
   }
 }
