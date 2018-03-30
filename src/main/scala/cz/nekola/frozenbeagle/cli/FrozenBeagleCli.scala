acb2682d96afc264cb262347ccaaf816a2c0fa40
Ondřej satai Nekola <ondra@nekola.cz>
2018-03-30 17:47:23 +0200

    Crude executable to see beagle running

---
+++ src/main/scala/cz/nekola/frozenbeagle/cli/FrozenBeagleCli.scala
@@ -0,0 +1,39 @@
+package cz.nekola.frozenbeagle.cli
+
+import java.lang.System.currentTimeMillis
+
+import cz.nekola.frozenbeagle.DnaString.randomDnaString
+import cz.nekola.frozenbeagle._
+
+import scala.util.Random
+
+object FrozenBeagleCli {
+
+  def randomIndividual: Individual = Individual(
+      sex = if (Random.nextBoolean()) F else M
+    , birthGeneration = 0
+    , chromosomes = ( randomDnaString, randomDnaString)
+  )
+
+  def main(args: Array[String]): Unit = {
+    val evolutionRules = EvolutionRules(maximumAge = 64,  populationSize = 1024 * 4)
+    val tng = Evolution.step(evolutionRules) _
+
+    val initialPopulation = Population( 0
+                                      , (1 to 1024).map(_ => randomIndividual).toSet
+                                      )
+
+    val ts = currentTimeMillis
+
+    val result = (1 to 1024).scanLeft(initialPopulation)((pop, _) => tng(pop))
+
+    println("Time: " + ((currentTimeMillis - ts) / 1000))
+
+    println(result(1).size)
+    println(result(512).size)
+    println(result(570).size)
+    println(result(1024).size)
+    println(result.last.size)
+  }
+
+}
