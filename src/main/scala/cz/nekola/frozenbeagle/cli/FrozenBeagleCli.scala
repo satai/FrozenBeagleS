202bf387d8fa7e8576ad14725e5921eb7e46d8fa
Ondra Nekola <ondra@nekola.cz>
2018-04-21 13:49:10 +0200

    Command line parameters in a separate files

--- src/main/scala/cz/nekola/frozenbeagle/cli/FrozenBeagleCli.scala
+++ src/main/scala/cz/nekola/frozenbeagle/cli/FrozenBeagleCli.scala
@@ -5,9 +5,7 @@ import java.lang.System.currentTimeMillis
 import cz.nekola.frozenbeagle.DnaString.randomDnaString
 import cz.nekola.frozenbeagle.SimulationConstants.{epochCount, epochLength}
 import cz.nekola.frozenbeagle._
-import org.rogach.scallop.ScallopConf
 
-import scala.collection.immutable
 import scala.util.Random
 
 
@@ -53,52 +51,6 @@ object FrozenBeagleCli {
           a ++ b
         }
   )
-
-  case class Params(arguments: Seq[String]) extends ScallopConf(arguments) {
-    val simulationCount = opt[Int] (
-        required = true
-      , descr = "Number of simulations to run with given set of parameters."
-      , noshort = true
-      )
-
-    val populationSize = opt[Int] (
-        required = true
-      , descr = "Size of initial population and the size of population, that is equlibrium for turbidostat."
-      , noshort = true
-      )
-
-    val countOfBases = opt[Int](
-        required = true
-      , descr = "DNA string length (count of allellas)."
-      , noshort = true
-      )
-
-    val ratioOfNegativeDominantRules = opt[Int](
-        required = true
-      , descr = "Ratio of negative dominant allelas as they appear in the initial population and as they are created by mutations "
-      , noshort = true
-      )
-
-    val ratioOfPleiotropicRules = opt[Int](
-        required = true
-      , descr = "Ratio of allellas that affect multiple dimensions of the phenotype. This ratio is in the initial population and in this ratio new allellas are created by mutation."
-      , noshort = true
-      )
-
-    val maximumAge = opt[Int](
-        required = true
-      , descr = "After this age organisms die by age."
-      , noshort = true
-    )
-
-    val mutationProbability = opt[Int](
-      required = true
-      , descr = "FIXME"
-      , noshort = true
-    )
-
-    verify()
-  }
 
   def main(args: Array[String]): Unit = {
 
